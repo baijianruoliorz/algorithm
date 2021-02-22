04c7b0e3517496a0ebc5295aa0c1366587c03c63
liqiqiorz <1099462011@qq.com>
2021-02-22 11:52:37 +0800

    feat:T34

---
+++ src/leetCode/day22/T34.java
@@ -0,0 +1,32 @@
+package leetCode.day22;
+
+/**
+ * @author liqiqi_tql
+ * @date 2021/2/22 -11:06
+ */
+public class T34 {
+    class Solution {
+        public int[] searchRange(int[] nums, int target) {
+            int leftIdx = binarySearch(nums, target, true);
+            int rightIdx = binarySearch(nums, target, false) - 1;
+            if (leftIdx <= rightIdx && rightIdx < nums.length && nums[leftIdx] == target && nums[rightIdx] == target) {
+                return new int[]{leftIdx, rightIdx};
+            }
+            return new int[]{-1, -1};
+        }
+
+        public int binarySearch(int[] nums, int target, boolean lower) {
+            int left = 0, right = nums.length - 1, ans = nums.length;
+            while (left <= right) {
+                int mid = (left + right) / 2;
+                if (nums[mid] > target || (lower && nums[mid] >= target)) {
+                    right = mid - 1;
+                    ans = mid;
+                } else {
+                    left = mid + 1;
+                }
+            }
+            return ans;
+        }
+    }
+}
