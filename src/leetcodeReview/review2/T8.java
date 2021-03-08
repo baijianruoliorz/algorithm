186bef176a27c8634a517738473c4cb918460b04
liqiqiorz <1099462011@qq.com>
2021-03-08 11:09:51 +0800

    feat:T8

---
+++ src/leetcodeReview/review2/T8.java
@@ -0,0 +1,32 @@
+package leetcodeReview.review2;
+
+/**
+ * @author liqiqi_tql
+ * @date 2021/3/8 -10:48
+ */
+public class T8 {
+    public int myAtoi(String str){
+        str = str.trim();
+        if (str.length()==0){
+            return 0;
+        }
+        if (!Character.isDigit(str.charAt(0))&&str.charAt(0)!='-'&&str.charAt(0)!='+'){
+            return 0;
+        }
+        long ans=0L;
+        boolean neg=str.charAt(0)=='-';
+        int i=!Character.isDigit(str.charAt(0))?1:0;
+        while (i<str.length()&&Character.isDigit(str.charAt(i))){
+            ans=ans*10+(str.charAt(i++)-'0');
+            if (!neg&&ans>Integer.MAX_VALUE){
+                ans=Integer.MAX_VALUE;
+                break;
+            }
+            if (neg&&ans>1L+Integer.MAX_VALUE){
+                ans=1L+Integer.MAX_VALUE;
+                break;
+            }
+        }
+        return neg?(int)-ans:(int) ans;
+    }
+}
