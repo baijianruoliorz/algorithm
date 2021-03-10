1462f99acdf88e90b23d1c720dd7ade07f53f528
liqiqiorz <1099462011@qq.com>
2021-03-10 21:07:18 +0800

    feat:T124

--- src/leetCode/day33/T124.java
+++ src/leetCode/day33/T124.java
@@ -7,7 +7,25 @@ import leetCode.day5.TreeNode;
  * @date 2021/3/10 -20:48
  */
 public class T124 {
+    int max=Integer.MIN_VALUE;
     public int maxPathSum(TreeNode root){
+        if (root==null){
+            return 0;
+        }
+        dfs(root);
+        return max;
+    }
+
+    private int dfs(TreeNode root) {
+        if (root==null){
+            return 0;
+        }
+        int left=Math.max(0,dfs(root.left));
+        int right = Math.max(0, dfs(root.right));
+        max=Math.max(max,root.val+left+right);
+        return root.val+Math.max(left,right);
+
+
 
     }
 }
