be595f842f413bba1c43c9f411df27c6f23c18f1
liqiqiorz <1099462011@qq.com>
2021-03-13 16:01:34 +0800

    feat:T101

---
+++ src/leetcodeReview/reviewTree/T101.java
@@ -0,0 +1,30 @@
+package leetcodeReview.reviewTree;
+
+import leetCode.day5.TreeNode;
+import sun.reflect.generics.tree.Tree;
+
+/**
+ * @author liqiqi_tql
+ * @date 2021/3/13 -15:49
+ */
+public class T101 {
+    public boolean isSymmetric(TreeNode root){
+        if (root==null||root.right==null&&root.left==null){
+            return true;
+        }
+        return dfs(root.left,root.right);
+    }
+
+    private boolean dfs(TreeNode left, TreeNode right) {
+        if (left==null&&right==null){
+            return true;
+        }
+        if (left==null||right==null){
+            return false;
+        }
+        if (left.val!=right.val){
+            return false;
+        }
+        return dfs(left.left,right.right)&&dfs(left.right,right.left);
+    }
+}
