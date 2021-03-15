1a0dff8deb3789a30b6946c5388c6382c32d0304
liqiqiorz <1099462011@qq.com>
2021-03-15 10:20:26 +0800

    feat:T142

--- src/leetcodeReview/reviewTree/T142.java
+++ src/leetcodeReview/reviewTree/T142.java
@@ -1,8 +1,29 @@
 package leetcodeReview.reviewTree;
 
+import leetCode.day4.ListNode;
+
 /**
  * @author liqiqi_tql
  * @date 2021/3/15 -10:12
  */
 public class T142 {
+    public ListNode detectCycle(ListNode head){
+        ListNode p=head,q=head;
+        while (true){
+            if (p==null||p.next==null){
+                return null;
+            }
+            p=p.next.next;
+            q=q.next;
+            if (p==q){
+                break;
+            }
+        }
+        p=head;
+        while (p!=q){
+            q=q.next;
+            p=p.next;
+        }
+        return p;
+    }
 }
