f0db9199e09c0e5017ee7acc4c604d16959aeb57
liqiqiorz <1099462011@qq.com>
2021-03-15 10:35:34 +0800

    feat:T102

---
+++ src/leetcodeReview/reviewTree/T102.java
@@ -0,0 +1,38 @@
+package leetcodeReview.reviewTree;
+
+import leetCode.day5.TreeNode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * @author liqiqi_tql
+ * @date 2021/3/15 -10:20
+ */
+public class T102 {
+    public List<List<Integer>> levelOrder(TreeNode root){
+        List<List<Integer>> res=new ArrayList<>();
+        Queue<TreeNode> queue=new LinkedList<>();
+        if (root!=null){
+            queue.add(root);
+        }
+        while (!queue.isEmpty()){
+            int n=queue.size();
+            ArrayList<Integer> list=new ArrayList<>();
+            for (int i=0;i<n;i++){
+                TreeNode node =queue.poll();
+                list.add(node.val);
+                if (node.left!=null){
+                    queue.add(node.left);
+                }
+                if(node.right!=null){
+                    queue.add(node.right);
+                }
+            }
+            res.add(list);
+        }
+        return res;
+    }
+}
