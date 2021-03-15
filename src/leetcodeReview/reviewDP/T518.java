46c6e8b38d9b7ddf9d03cb4f235ca9e026c2b434
liqiqiorz <1099462011@qq.com>
2021-03-15 17:14:33 +0800

    feat:T518

---
+++ src/leetcodeReview/reviewDP/T518.java
@@ -0,0 +1,18 @@
+package leetcodeReview.reviewDP;
+
+/**
+ * @author liqiqi_tql
+ * @date 2021/3/15 -16:14
+ */
+public class T518 {
+    public int change(int amount,int[] coins){
+     int[] dp=new int[amount+1];
+     dp[0]=1;
+     for (int i=0;i<coins.length;i++){
+         for (int j=coins[i];j<=amount;j++){
+             dp[j]+=dp[j-coins[i]];
+         }
+     }
+     return dp[amount];
+    }
+}
