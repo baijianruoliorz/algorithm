ec334d4bf533b4b56f54569765e9eacb6de1daf2
liqiqiorz <1099462011@qq.com>
2021-03-16 21:17:49 +0800

    feat:T236

---
+++ src/leetcodeReview/reviewTree/T236.java
@@ -0,0 +1,24 @@
+package leetcodeReview.reviewTree;
+
+import leetCode.day5.TreeNode;
+
+/**
+ * @author liqiqi_tql
+ * @date 2021/3/16 -21:07
+ */
+public class T236 {
+    public TreeNode lowestCommonAncestor(TreeNode root,TreeNode p,TreeNode q){
+        if (root==null||root==p||root==q){
+            return root;
+        }
+        TreeNode left=lowestCommonAncestor(root.left,p,q);
+        TreeNode right=lowestCommonAncestor(root.right,p,q);
+        if (left==null) {
+            return right;
+        }
+        if (right==null){
+            return left;
+        }
+        return root;
+    }
+}
