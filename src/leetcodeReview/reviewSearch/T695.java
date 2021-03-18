a2109c3c07fe5c444a9b30c12660f27427e6daaf
liqiqiorz <1099462011@qq.com>
2021-03-18 08:52:31 +0800

    feat:T695

---
+++ src/leetcodeReview/reviewSearch/T695.java
@@ -0,0 +1,32 @@
+package leetcodeReview.reviewSearch;
+
+/**
+ * @author liqiqi_tql
+ * @date 2021/3/17 -11:28
+ */
+public class T695 {
+    public int maxAreaOfIsland(int[][] grid){
+        int ans=0;
+        for (int i=0;i<grid.length;i++){
+            for (int j=0;j<grid[0].length;j++){
+                ans=Math.max(ans,dfs(grid,i,j));
+            }
+        }
+        return ans;
+    }
+
+    private int dfs(int[][] grid, int cur_i, int cur_j) {
+        if (cur_i<0||cur_j<0||cur_i==grid.length||cur_j==grid[0].length||grid[cur_i][cur_j]!=1){
+            return 0;
+        }
+        grid[cur_i][cur_j]=0;
+        int[] di={0,0,-1,1};
+        int[] dj={1,-1,0,0};
+        int ans=1;
+        for (int index=0;index!=4;++index){
+            int next_i=cur_i+di[index],next_j=cur_j+dj[index];
+            ans+=dfs(grid,next_i,next_j);
+        }
+        return ans;
+    }
+}
