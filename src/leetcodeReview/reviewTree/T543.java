e158ec7f95c096dfe74f04fe904a210a712616ce
liqiqiorz <1099462011@qq.com>
2021-03-19 21:20:00 +0800

    feat:T543

---
+++ src/leetcodeReview/reviewTree/T543.java
@@ -0,0 +1,26 @@
+package leetcodeReview.reviewTree;
+
+import leetCode.day5.TreeNode;
+
+/**
+ * @author liqiqi_tql
+ * @date 2021/3/19 -20:15
+ */
+public class T543 {
+    int ans;
+    public int diameterOfBinaryTree(TreeNode root){
+        ans=1;
+        depth(root);
+        return ans-1;
+    }
+
+    private int depth(TreeNode root) {
+        if (root==null){
+            return 0;
+        }
+        int left=depth(root.left);
+        int right=depth(root.right);
+        ans=Math.max(ans,right+left+1);
+        return Math.max(left,right)+1;
+    }
+}
