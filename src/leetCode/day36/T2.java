0c567e8d308eff8ffa5589225c66d710b071e803
liqiqiorz <1099462011@qq.com>
2021-03-27 09:21:36 +0800

    feat:LRU

--- src/leetCode/day36/T2.java
+++ src/leetCode/day36/T2.java
@@ -8,34 +8,33 @@ import leetCode.day4.ListNode;
  */
 public class T2 {
     public ListNode addTwoNumbers(ListNode l1,ListNode l2){
-        reverseList(l1);
-        reverseList(l2);
+        ListNode l3=reverseList(l1);
+        ListNode l4 = reverseList(l2);
         int sum1=0;
         int sum2=0;
-        while (l1!=null){
+        while (l3!=null){
             if (sum1!=0){
                 sum1*=10;
             }
-            sum1+=l1.val;
-            l1=l1.next;
+            sum1+=l3.val;
+            l3=l3.next;
         }
-        while (l2!=null){
+        while (l4!=null){
             if (sum2!=0){
                 sum2*=10;
             }
-            sum2+=l2.val;
-            l2=l2.next;
+            sum2+=l4.val;
+            l4=l4.next;
         }
         ListNode list=new ListNode(0);
+        ListNode pre=list;
         int sum3=sum1+sum2;
         while (sum3>0){
             int s=sum3%10;
-            list.val=s;
-            list.next=null;
-            list=list.next;
+            pre.next=new ListNode(s);
             sum3/=10;
         }
-        return list;
+        return pre.next;
 
     }
     public ListNode reverseList(ListNode root){
