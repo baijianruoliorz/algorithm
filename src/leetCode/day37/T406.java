18f9f767e240047b34dfa781ab9bc66bc249cf65
liqiqiorz <1099462011@qq.com>
2021-04-01 16:54:38 +0800

    feat:T406

--- src/leetCode/day37/T406.java
+++ src/leetCode/day37/T406.java
@@ -1,11 +1,23 @@
 package leetCode.day37;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * @author liqiqi_tql
  * @date 2021/4/1 -8:43
  */
 public class T406 {
     public int[][] reconstructQueue(int[][] people){
-
+        if (people==null||people.length==0||people[0].length==0){
+            return new int[0][0];
+        }
+        Arrays.sort(people,(a,b)->(a[0]==b[0])?a[1]-b[1]:b[0]-a[0]);
+        List<int[]> queue=new ArrayList<>();
+        for (int[] p:people){
+            queue.add(p[1],p);
+        }
+        return queue.toArray(new int[queue.size()][]);
     }
 }
