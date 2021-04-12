b225231ec888b1550e5cda7a405e24effa0925d3
liqiqiorz <1099462011@qq.com>
2021-04-12 21:42:23 +0800

    feat:T16

---
+++ src/leetCode/day41/t16.java
@@ -0,0 +1,33 @@
+package leetCode.day41;
+
+import java.util.Arrays;
+
+/**
+ * @author liqiqi_tql
+ * @date 2021/4/12 -21:38
+ */
+public class t16 {
+    public int threeSumClosest(int[] nums,int target){
+        int length = nums.length;
+        Arrays.sort(nums);
+        int ans=nums[0]+nums[1]+nums[2];
+        for (int i=0;i<length;i++){
+            int start=i+1;
+            int end=length-1;
+            while (start<end){
+                int sum=nums[i]+nums[start]+nums[end];
+                if (Math.abs(target-sum)<Math.abs(target-ans)){
+                    ans=sum;
+                }
+                if (sum>target){
+                    end--;
+                }else if (sum<target){
+                    start++;
+                }else {
+                    return ans;
+                }
+            }
+        }
+        return ans;
+    }
+}
