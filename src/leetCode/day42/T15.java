ab72fc6f388d274dfde6eaccb42b071e7944fd83
liqiqiorz <1099462011@qq.com>
2021-04-12 22:00:57 +0800

    feat:T15

---
+++ src/leetCode/day42/T15.java
@@ -0,0 +1,51 @@
+package leetCode.day42;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author liqiqi_tql
+ * @date 2021/4/12 -21:44
+ */
+public class T15 {
+    public List<List<Integer>> threeSum(int[] nums){
+        List<List<Integer>> lists=new ArrayList<>();
+        Arrays.sort(nums);
+        int len = nums.length;
+        for (int i=0;i<len;i++){
+            if (nums[i]>0){
+                return lists;
+            }
+            if (i>0&&nums[i]==nums[i-1]){
+                continue;
+            }
+            int cur=nums[i];
+            int L=i+1;
+            int R=len-1;
+            while (L < R ) {
+                int tmp=cur+nums[R]+nums[L];
+                if (tmp==0){
+                    List<Integer> list=new ArrayList<>();;
+                    list.add(cur);
+                    list.add(nums[R]);
+                    list.add(nums[L]);
+                    lists.add(list);
+                    while (L<R&&nums[L+1]==nums[L]){
+                        L++;
+                    }
+                    while (L<R&&nums[R-1]==nums[R]){
+                        R--;
+                    }
+                    L++;
+                    R--;
+                }else if (tmp<0){
+                    L++;
+                }else {
+                    R--;
+                }
+            }
+        }
+        return lists;
+    }
+}
