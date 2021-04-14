7a197977ea58a50ab54bbc911385b4907a26003b
liqiqiorz <1099462011@qq.com>
2021-04-14 21:05:19 +0800

    feat:Tm1721

---
+++ src/leetCode/day43/Tm1721.java
@@ -0,0 +1,32 @@
+package leetCode.day43;
+
+/**
+ * @author liqiqi_tql
+ * @date 2021/4/14 -21:04
+ */
+public class Tm1721 {
+    public int trap(int[] height){
+        int sum=0;
+        for (int i=1;i<height.length-1;i++){
+            int maxLeft=0;
+            for(int j=i-1;j>=0;j--){
+                if(height[j]>maxLeft){
+                    maxLeft=height[j];
+                }
+            }
+            int maxRight=0;
+            for(int j=i+1;j<height.length;j++){
+                if(height[j]>maxRight){
+                    maxRight=height[j];
+                }
+            }
+            int min=Math.min(maxLeft,maxRight);
+            if(min>height[i]){
+                sum=sum+(min-height[i]);
+            }
+        }
+        return sum;
+
+        }
+    }
+
