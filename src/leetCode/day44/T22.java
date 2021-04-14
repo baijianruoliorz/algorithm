48d76014a0e9dd853dd61750fe0f9ff793565f99
liqiqiorz <1099462011@qq.com>
2021-04-14 21:09:35 +0800

    feat:T22

---
+++ src/leetCode/day44/T22.java
@@ -0,0 +1,35 @@
+package leetCode.day44;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author liqiqi_tql
+ * @date 2021/4/14 -21:06
+ */
+public class T22 {
+    public List<String> generateParenthesis(int n){
+        List<String> res=new ArrayList<>();
+        if (n==0){
+            return res;
+        }
+        dfs("",0,0,n,res);
+        return res;
+    }
+
+    private void dfs(String curStr, int left, int right, int n, List<String> res) {
+        if (left==n&&right==n){
+            res.add(curStr);
+            return;
+        }
+        if (left<right){
+            return;
+        }
+        if (left<n){
+            dfs(curStr+"(",left+1,right,n,res);
+        }
+        if (right<n){
+            dfs(curStr+")",left,right+1,n,res);
+        }
+    }
+}
