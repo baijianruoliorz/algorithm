c8a8489756f26005262216a61491868b2573dc21
liqiqiorz <1099462011@qq.com>
2021-04-15 15:24:29 +0800

    feat:T103

---
+++ src/leetCode/day45/T103.java
@@ -0,0 +1,45 @@
+package leetCode.day45;
+
+import leetCode.day5.TreeNode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * @author liqiqi_tql
+ * @date 2021/4/15 -15:14
+ */
+public class T103 {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root){
+        List<List<Integer>> res=new ArrayList<>();
+        if (root==null){
+            return  res;
+        }
+        Queue<TreeNode> queue=new LinkedList<>();
+        queue.add(root);
+        boolean leftToRight=true;
+        while (!queue.isEmpty()){
+            List<Integer> level=new ArrayList<>();
+            int count = queue.size();
+            for (int i=0;i<count;i++){
+                TreeNode node=queue.poll();
+                if (leftToRight){
+                    level.add(node.val);
+                }else {
+                    level.add(0,node.val);
+                }
+                if (node.left!=null){
+                    queue.add(node.left);
+                }
+                if (node.right != null) {
+                    queue.add(node.right);
+                }
+            }
+            res.add(level);
+            leftToRight=!leftToRight;
+        }
+        return res;
+    }
+}
