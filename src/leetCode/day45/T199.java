05c82153bdbc20f0b7104f4e3e70b3a807d75a22
liqiqiorz <1099462011@qq.com>
2021-04-15 15:13:01 +0800

    feat:T199

---
+++ src/leetCode/day45/T199.java
@@ -0,0 +1,41 @@
+package leetCode.day45;
+
+import leetCode.day5.TreeNode;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
+
+/**
+ * @author liqiqi_tql
+ * @date 2021/4/15 -15:05
+ */
+public class T199 {
+    public List<Integer> rightSideView(TreeNode root){
+        List<Integer> res=new ArrayList<>();
+        if (root==null){
+            return res;
+        }
+        Deque<TreeNode> treeDeque=new ArrayDeque<>();
+        treeDeque.add(root);
+        while (!treeDeque.isEmpty()){
+            int size=treeDeque.size();
+            List<Integer> treeList = new ArrayList<>();
+            for (int i=0;i<size;i++){
+                TreeNode poll = treeDeque.poll();
+                if (i==size-1){
+                    treeList.add(poll.val);
+                }
+                if (poll.left!=null){
+                    treeDeque.add(poll.left);
+                }
+                if (poll.right!=null){
+                    treeDeque.add(poll.right);
+                }
+            }
+            res.addAll(treeList);
+        }
+        return res;
+    }
+}
