d67c656159aaa19696477c9401f32490e3e1ee7b
liqiqiorz <1099462011@qq.com>
2021-04-16 09:32:36 +0800

    feat:T304

--- src/leetCode/day46/T341.java
+++ src/leetCode/day46/T341.java
@@ -13,11 +13,6 @@ public class T341 {
         private List<Integer> vals;
         private Iterator<Integer> cur;
 
-        public NestedIterator(List<NestedInteger> nestedList) {
-            vals = new ArrayList<Integer>();
-            dfs(nestedList);
-            cur = vals.iterator();
-        }
 
         @Override
         public Integer next() {
@@ -29,14 +24,6 @@ public class T341 {
             return cur.hasNext();
         }
 
-        private void dfs(List<NestedInteger> nestedList) {
-            for (NestedInteger nest : nestedList) {
-                if (nest.isInteger()) {
-                    vals.add(nest.getInteger());
-                } else {
-                    dfs(nest.getList());
-                }
-            }
-        }
+
     }
 }
