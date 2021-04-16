5ed126c8a47a8b601c894cda0e7f1bf6f44c7f60
liqiqiorz <1099462011@qq.com>
2021-04-16 15:40:55 +0800

    feat:T403

---
+++ src/leetCode/day46/T403.java
@@ -0,0 +1,29 @@
+package leetCode.day46;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author liqiqi_tql
+ * @date 2021/4/16 -15:33
+ */
+public class T403 {
+    public boolean canCross(int[] stones){
+        HashMap<Integer, Set<Integer>> map=new HashMap<>();
+        for (int i=0;i<stones.length;i++){
+            map.put(stones[i],new HashSet<Integer>());
+        }
+        map.get(0).add(0);
+        for (int i=0;i<stones.length;i++){
+            for (Integer k : map.get(stones[i])) {
+                for (int step=k-1;step<=k+1;step++){
+                    if (step>0&&map.containsKey(stones[i]+step)){
+                        map.get(stones[i]+step).add(step);
+                    }
+                }
+            }
+        }
+        return map.get(stones[stones.length-1]).size()>0;
+    }
+}
